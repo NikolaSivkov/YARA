8806463bccbbd80ecce3dccfbcadb74128dd345d
Nikola Sivkov <erasrz@gmail.com>
2015-10-10 22:01:18 +0300

    First working version of YARA

---
+++ YARA/YARA/CodeFixProvider.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.Rename;
+using Microsoft.CodeAnalysis.Text;
+
+namespace YARA
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(YARACodeFixProvider)), Shared]
+    public class YARACodeFixProvider : CodeFixProvider
+    {
+
+        public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(YARAAnalyzer.DiagnosticId);
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+             
+            var memberAccessNode = root.FindToken(diagnosticSpan.Start).Parent.FirstAncestorOrSelf<MemberAccessExpressionSyntax>();
+
+            var semanticmodel = await context.Document.GetSemanticModelAsync(context.CancellationToken).ConfigureAwait(false);
+
+            var method = semanticmodel.GetEnclosingSymbol(memberAccessNode.SpanStart) as IMethodSymbol;
+
+
+            if (method != null && method.IsAsync)
+            {
+                var invokeMethod = semanticmodel.GetSymbolInfo(memberAccessNode).Symbol as IMethodSymbol;
+
+                if (invokeMethod != null)
+                {
+                    var invocation = memberAccessNode.FirstAncestorOrSelf<InvocationExpressionSyntax>();
+
+                    if (!memberAccessNode.Name.Identifier.Text.Contains("Async"))
+                    {
+                        var name = memberAccessNode.Name.Identifier.Text;
+
+                        // Register a code action that will invoke the fix.
+                        context.RegisterCodeFix(
+                            new CodeActionChangetoAwaitAsync("Swap with Async",
+                                c => ChangetoAwaitAsync(context.Document, invocation, name, c)),
+                            diagnostic);
+                        return;
+                    }
+                }
+            }
+        }
+
+        private async Task<Document> ChangetoAwaitAsync(Document document, InvocationExpressionSyntax invocation, string name, CancellationToken cancellationToken)
+        {
+            SyntaxNode oldExpression = invocation;
+            SyntaxNode newExpression = null;
+
+
+            if (!name.Contains("Async"))
+            {
+                var oldToken = invocation.DescendantTokens().First(z => z.Text == name);
+                 
+                var newToken = SyntaxFactory.Identifier(oldToken.LeadingTrivia,SyntaxKind.IdentifierToken,name + "Async", name + "Async", oldToken.TrailingTrivia);
+
+                var tempInvocation = invocation.ReplaceToken(oldToken, newToken);
+                newExpression = SyntaxFactory.AwaitExpression(tempInvocation).WithAdditionalAnnotations(Formatter.Annotation);
+            }
+
+            var oldroot = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            var newroot = oldroot.ReplaceNode(oldExpression, newExpression);
+
+            var newDocument = document.WithSyntaxRoot(newroot);
+
+            return newDocument;
+        }
+    }
+
+
+    public class CodeActionChangetoAwaitAsync : CodeAction
+    {
+        private Func<CancellationToken, Task<Document>> _generateDocument;
+        private string _title;
+
+        public CodeActionChangetoAwaitAsync(string title, Func<CancellationToken, Task<Document>> generateDocument)
+        {
+            _title = title;
+            _generateDocument = generateDocument;
+        }
+
+        public override string Title { get { return _title; } }
+
+        protected override Task<Document> GetChangedDocumentAsync(CancellationToken cancellationToken)
+        {
+            return _generateDocument(cancellationToken);
+        }
+
+        public override string EquivalenceKey
+        {
+            get
+            {
+                return nameof(CodeActionChangetoAwaitAsync);
+            }
+        }
+    }
+
+}
---
+++ YARA/YARA/DiagnosticAnalyzer.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+namespace YARA
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class YARAAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "YARA001-EFAsync";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Async Helper";
+
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
+            context.RegisterSyntaxNodeAction(c => AnalyzeMethodNode(c), SyntaxKind.SimpleMemberAccessExpression);
+
+        }
+
+        private const string _efNamespace = "System.Data.Entity";
+
+        /// <summary>
+        /// namespace is a key, value is a ist of symbols
+        /// </summary>
+        private static ConcurrentBag<ISymbol> _names = new ConcurrentBag<ISymbol>();
+
+        private void AnalyzeMethodNode(SyntaxNodeAnalysisContext context)
+        {
+
+            var memberAccessNode = (MemberAccessExpressionSyntax)context.Node;
+
+
+            // this gets the method that hosts 
+            var method = context.SemanticModel.GetEnclosingSymbol(context.Node.SpanStart) as IMethodSymbol;
+
+            if (method != null && method.IsAsync)
+            {
+
+                var invokeMethod = context.SemanticModel.GetSymbolInfo(context.Node).Symbol as IMethodSymbol;
+
+                var location = memberAccessNode.Name.GetLocation();
+                if (invokeMethod != null)
+                {
+                    var containingNamespace = invokeMethod.ContainingNamespace.ToString();
+
+                    if (!invokeMethod.IsAsync &&
+                        (containingNamespace == _efNamespace || containingNamespace == "System.Linq") &&
+                        (invokeMethod.ReturnType?.BaseType?.Name ?? "") != "Task")
+                    {
+
+                        //if the containing name space is EFs namespace we can directly proceed to add all methods to our list and generate diagnostic if it has async counterpart
+                        if (containingNamespace == _efNamespace)
+                        {
+                            AddNamespace(invokeMethod.ContainingNamespace);
+                            
+                            if (HasMethod(invokeMethod.Name))
+                            {
+                                context.ReportDiagnostic(Diagnostic.Create(Rule, location, invokeMethod.Name));
+                                return;
+                            }
+
+                        }
+
+                        var parentExp = memberAccessNode.Expression;
+                        var parentNode = parentExp.Parent.FindNode(parentExp.Span);
+                        var parentIsEfType = false;
+
+                        //if the sibling is a property ( DB.myTable ) 
+                        var parentSymbolAsProperty = context.SemanticModel.GetSymbolInfo(parentNode).Symbol as IPropertySymbol;
+
+                        if (parentSymbolAsProperty != null && parentSymbolAsProperty.Type.Interfaces.Any(x => x.ContainingNamespace.ToString() == _efNamespace))
+                        {
+                            parentIsEfType = true;
+                        }
+
+                        //if the sibling is another linq method that returns IQueriable
+                        var parentSymbolAsMethod = context.SemanticModel.GetSymbolInfo(parentNode).Symbol as IMethodSymbol;
+
+                        if (parentSymbolAsMethod != null && parentSymbolAsMethod.IsExtensionMethod && parentSymbolAsMethod.IsGenericMethod && parentSymbolAsMethod.ContainingType.ToString() == _efNamespace)
+                        {
+                            parentIsEfType = true;
+                        }
+                        
+                        if (HasMethod(invokeMethod.Name) && parentIsEfType)
+                        {
+                            context.ReportDiagnostic(Diagnostic.Create(Rule, location, invokeMethod.Name));
+                        }
+                    }
+                }
+            }
+        }
+
+        private void AddNamespace(INamespaceSymbol namespaceSymbol)
+        {
+            if (_names.IsEmpty) return;
+
+            var classList = namespaceSymbol.GetMembers();
+
+            var methodsList = classList.ToList().SelectMany(x => x.GetMembers());
+
+            var methods = new List<ISymbol>(methodsList);
+            try
+            {
+                _names = new ConcurrentBag<ISymbol>(methods);
+            }
+            catch
+            {
+            }
+        }
+
+        private bool HasMethod(string name)
+        {
+            return _names.Any(z => z.Name == name + "Async");
+        }
+    }
+}
